f9757883f0d4e07425dd9544812a93ec572dd1e9
Amna Zahid <amnazahid399@gmail.com>
2026-03-04 10:52:34 +0500

    :trade: Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#Langchain Chatbot
+# Langchain Chatbot
---
+++ app.py
@@ -0,0 +1,9 @@
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(
+    model="minimax-m2.5:cloud",
+    temperature=0.7,
+
+)
+response = llm.invoke("What is RAG?") #invoke gives AI message, & returns a coplete class
+print(response.content) 
